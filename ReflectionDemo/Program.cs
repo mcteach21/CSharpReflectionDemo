0b76c7f650050ef2e30578808e17041f2c91b777
MC-Teacher <mc.teach.21@gmail.com>
2021-12-10 10:38:20 +0100

    Ajoutez des fichiers projet.

---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******************************************");
+            ReflectionUtil.FromClass("DummyClass");
+
+            Console.WriteLine("******************************************");
+        }
+    }
+}
